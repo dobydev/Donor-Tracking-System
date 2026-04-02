b4cf0971e4c8baa04ad60ad5bef748f3d0f2f5a8
Gabriel <gabriel.doby21@gmail.com>
2026-04-02 16:16:27 -0500

    Configure Identity, DbContext, and session
    Added ApplicationUser and ApplicationDbContext models and a simple AccountLoginViewModel.
    Configured EF Core to use a LocalDB SQL Server database and register Identity with EF stores and default token providers. Enabled distributed memory cache and session support, and update Program.cs middleware to include authentication/authorization and session wiring. Also forgot to Add Microsoft.SqlServer.Server package reference.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,36 @@
+/* 
+   Alex Alvarado, Gabriel Doby, Prabesh Khanal, 
+   Justin Kim, Destini Liphart, Nursang Sherpa, Jerome Whitaker
+   Group 1: Team Project
+   CISS 491: Business Software Development
+ */
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using DonorTrackingSystem.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Database connection string
+var connection = $"Server=(localdb)\\mssqllocaldb;Database=DonorTrackingSystemDb;" +
+    $"Trusted_Connection=True;MultipleActiveResultSets=true";
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(connection));
+
+// Identity configuration
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -9,16 +38,21 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+// Middleware configuration
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseRouting();
 app.UseAuthorization();
+app.UseAuthorization();
+app.UseSession();
+
+
 
 app.MapControllerRoute(
     name: "default",
